52c80d6fe77c8582f07c053e212ed9094b8112b0
Yash Patel <yashpatelit024@gmail.com>
2024-03-21 08:08:26 -0400

    Added Vertical timeline component for experience

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "../../constants/data";
 
 const StyledExperienceSection = styled.section`
   display: flex;
@@ -12,8 +18,19 @@ const Experience = () => {
   return (
     <StyledExperienceSection id="experience">
       <h2>Experience</h2>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement date={item.date}>
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </StyledExperienceSection>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
